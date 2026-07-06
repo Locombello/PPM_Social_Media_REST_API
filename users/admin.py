84a89eec59cd50f2e1d9a78994064b58d88985b3
Locombello <colombani.lorenzo.19@gmail.com>
2026-07-07 00:54:43 +0200

    populate sqlite db with users

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    # Aggiungiamo i nostri campi custom alla pagina di modifica dell'utente
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informazioni Aggiuntive', {'fields': ('role', 'bio')}),
+    )
+    
+    # Opzionale: mostriamo il ruolo direttamente nella tabella riassuntiva degli utenti
+    list_display = ['username', 'email', 'role', 'is_staff']
+
+# Sostituiamo la registrazione di default con la nostra classe personalizzata
+admin.site.register(CustomUser, CustomUserAdmin)
